a7ab30f6fc6503f8bb91fdc29b462a3d06c8c5e0
CanRau <cansrau@gmail.com>
2020-02-18 00:11:50 -0500

    feat: add shared_preferences to persist theme

--- lib/ui/global/theme/bloc/theme_bloc.dart
+++ lib/ui/global/theme/bloc/theme_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../app_themes.dart';
 
@@ -11,8 +12,13 @@ part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
+  ThemeBloc() {
+    _loadSharedPrefs();
+  }
+
   @override
-  ThemeState get initialState => ThemeState(themeData: appThemeData[AppTheme.greenDark]);
+  ThemeState get initialState =>
+      ThemeState(themeData: appThemeData[AppTheme.greenDark]);
 
   @override
   Stream<ThemeState> mapEventToState(
@@ -20,6 +26,21 @@ class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   ) async* {
     if (event is ThemeChanged) {
       yield ThemeState(themeData: appThemeData[event.theme]);
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      // print('event.theme');
+      // print('${event.theme.hashCode}');
+      // print('${event.theme.index}');
+      // print('${event.theme.toString()}');
+      // print('event.theme');
+      prefs.setInt('theme', event.theme.index);
+    }
+  }
+
+  Future<void> _loadSharedPrefs() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final int themeIndex = prefs.get('theme') as int;
+    if (themeIndex != null) {
+      add(ThemeChanged(theme: AppTheme.values[themeIndex]));
     }
   }
 }
