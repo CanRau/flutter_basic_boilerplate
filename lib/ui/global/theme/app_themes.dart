121859469a6656bad06227542ebafa22396a971d
CanRau <cansrau@gmail.com>
2020-02-18 01:42:12 -0500

    fix: FAB backgroundColor

--- lib/ui/global/theme/app_themes.dart
+++ lib/ui/global/theme/app_themes.dart
@@ -8,8 +8,32 @@ enum AppTheme {
 }
 
 final Map<AppTheme, ThemeData> appThemeData = <AppTheme, ThemeData>{
-  AppTheme.greenLight: ThemeData(brightness: Brightness.light, primaryColor: Colors.green),
-  AppTheme.greenDark: ThemeData(brightness: Brightness.dark, primaryColor: Colors.green[700]),
-  AppTheme.blueLight: ThemeData(brightness: Brightness.light, primaryColor: Colors.blue),
-  AppTheme.blueDark: ThemeData(brightness: Brightness.dark, primaryColor: Colors.blue[700]),
+  AppTheme.greenLight: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.green,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.greenAccent,
+    ),
+  ),
+  AppTheme.greenDark: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.green[700],
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: Colors.greenAccent[700],
+    ),
+  ),
+  AppTheme.blueLight: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.blueAccent,
+    ),
+  ),
+  AppTheme.blueDark: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue[700],
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: Colors.blueAccent[700],
+    ),
+  ),
 };
