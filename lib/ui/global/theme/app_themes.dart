80bdc364a0e6338622c3cf96634f8e1471be7106
CanRau <cansrau@gmail.com>
2020-02-19 13:59:44 -0500

    fix: theme colors

--- lib/ui/global/theme/app_themes.dart
+++ lib/ui/global/theme/app_themes.dart
@@ -12,28 +12,28 @@ final Map<AppTheme, ThemeData> appThemeData = <AppTheme, ThemeData>{
     brightness: Brightness.light,
     primaryColor: Colors.green,
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: Colors.greenAccent,
+      backgroundColor: Colors.green,
     ),
   ),
   AppTheme.greenDark: ThemeData(
     brightness: Brightness.dark,
     primaryColor: Colors.green[700],
     floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: Colors.greenAccent[700],
+      backgroundColor: Colors.green[700],
     ),
   ),
   AppTheme.blueLight: ThemeData(
     brightness: Brightness.light,
     primaryColor: Colors.blue,
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Colors.blue,
     ),
   ),
   AppTheme.blueDark: ThemeData(
     brightness: Brightness.dark,
     primaryColor: Colors.blue[700],
     floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: Colors.blueAccent[700],
+      backgroundColor: Colors.blue[700],
     ),
   ),
 };
